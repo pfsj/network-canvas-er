11983570cca7006a2e4623540bcbb41a497bce74
pfsj <patrick.janulis@northwestern.edu>
2020-01-23 14:36:03 -0600

    Add decision trees and logistic regression

--- EntityResolution.py
+++ EntityResolution.py
@@ -3,6 +3,7 @@ import numpy as np
 import sys
 import recordlinkage
 
+# Currently reads in
 df  =  pd.read_csv("EntityResolution_attributeList.csv", delimiter=',',index_col='networkCanvasAlterID')
 
 # Merge dataframe to itself to get pairwise comparisons
@@ -16,7 +17,24 @@ comp_pairs.string('First Name', 'First Name', method='levenshtein',label='fnLeve
 comp_pairs.string('Last Name', 'Last Name', method='levenshtein',label='lnLevenDist')
 pairwise = comp_pairs.compute(index_list, df)
 
-pairwise["prob"] = pairwise.mean(axis=1)
+erAlgorithm = "simple"
+algorithmError = "Please specify an appropriate algorithm"
+if erAlgorithm == "simple": # Use simple mean of all string distances
+    pairwise["prob"] = pairwise.mean(axis=1)
+elif erAlgorithm == "logReg": # Use logistic regression
+    features = ['fnJwDist','fnLevenDist','lnJwDist','lnLevenDist']
+    from joblib import dump, load
+    from sklearn.linear_model import LogisticRegression
+    logRegModel = load('pugLogRegression.joblib')
+    pairwise['prob'] = logRegModel.predict_proba(pairwise[features])[:,1]
+elif erAlgorithm == "decisionTree":     # Use degression tree
+    features = ['fnJwDist','fnLevenDist','lnJwDist','lnLevenDist']
+    from joblib import dump, load
+    from sklearn.tree import DecisionTreeRegressor
+    treeModel = load('pugDecisionTree.joblib')
+    pairwise['prob'] = treeModel.predict(pairwise[features])
+else:
+    print(algorithmError)
 
 # Output edgelist w/ probability
 pairwise[['prob']].to_csv(sys.stdout,index=True)
