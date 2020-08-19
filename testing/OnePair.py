826f21dc35dded859d5309fbe7d4b2420d78ffd9
Steve Mckellar <steve@wwqrd.uk>
2020-08-19 16:11:22 +0100

    Update OnePair to filter by type

--- testing/OnePair.py
+++ testing/OnePair.py
@@ -5,22 +5,24 @@ import random
 lines = []
 
 for line in sys.stdin:
-    lines.append(line)
+  line_parts = line.rstrip().split(",")
+  entity_type = line_parts[1].rstrip()
+  if (entity_type == "4aebf73e-95e3-4fd1-95e7-237dcc4a4466"):
+    lines.append(line_parts)
+
 
 for index, line in enumerate(lines):
   if index == 0:
     print("networkCanvasAlterID_1, networkCanvasAlterID_2, prob, count", flush=True)
     continue
 
-  line_parts = line.rstrip().split(",")
-
   # For every unique combination:
   for index2, line2 in enumerate(lines):
     if index2 <= index:
       continue
 
-    line_parts2 = line2.rstrip().split(",")
-    print(f'{line_parts[0].rstrip()}, {line_parts2[0].rstrip()}, {prob}, {count}', flush=True)
+    prob = float(random.random()) # randomly assign pairs a probability
+    print(f'{line[0].rstrip()}, {line2[0].rstrip()}, {prob}', flush=True)
 
     break
 
